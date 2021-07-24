7386bef323dff81aad8919ecf6dedc4fd7d59582
alfZone <47450247+alfZone@users.noreply.github.com>
2021-07-24 17:41:46 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author alf
+ * @copyright 2020
+ */
+
+ 
+/* database access credencial;  */
+define("_AUTORES", 'alf');
+define("_BDUSER", 'user');
+define("_BDPASS", 'password');
+define("_BD", 'databasename');
+?>
