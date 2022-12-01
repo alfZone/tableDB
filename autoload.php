8066cfe4403c6674abf668dd967509a467266c10
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-12-01 12:02:13 +0000

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+spl_autoload_register(function ($class) {
+
+    $prefix = '';
+
+    $base_dir = __DIR__.'/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+
+    $file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+?>
